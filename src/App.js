a19d4d1f82c8215b4dd79d81f89c4cc0f6206bd4
salome <salomemumladze775@gmail.com>
2021-08-10 01:02:43 +0400

    done api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "Home";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import Theme from "Theme";
 import Layout from "./Layout";
-import Api from "./api";
 
 function App() {
   return (
